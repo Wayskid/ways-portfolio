47d1d2e77edbc08947e47213a9ce907702474587
Wayskid <wayskid02@gmail.com>
2023-07-25 22:04:47 +0100

    Minor upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Nav from "./layout/Nav";
 import Footer from "./layout/Footer";
 import { useEffect, useState } from "react";
 import logoVideo from "./assets/img/waysLogoAnim.mp4";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   const [showLogo, setShowLogo] = useState(true);
@@ -27,6 +28,21 @@ function App() {
         />
       ) : (
         <>
+          <AnimatedCursor
+            innerSize={8}
+            outerSize={45}
+            innerScale={1}
+            outerScale={2}
+            outerAlpha={0}
+            innerStyle={{
+              backgroundColor: "#cc2a21",
+            }}
+            outerStyle={{
+              border: "3px solid #cc2a21",
+            }}
+            showSystemCursor={true}
+            trailingSpeed={1}
+          />
           <Nav />
           <Home />
           <Footer />
